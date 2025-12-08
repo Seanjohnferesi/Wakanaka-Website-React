935346f31b1774e357a3381efb12952e5e65a430
Seanjohnferesi <seanjohnferesi27@gmail.com>
2025-12-08 21:55:38 +0200

    feat: add smooth page transitions using Framer Motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Home from './pages/Home'
 import Explore from "./pages/Explore"
-import {Routes, Route } from "react-router-dom"
+import PageWrapper from './components/PageWrapper';
+import {Routes, Route, useLocation } from "react-router-dom"
+import {AnimatePresence} from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path = "/explore" element={<Explore />} />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+        <Route path = "/explore" element={<PageWrapper><Explore /></PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom";
 import React from 'react';
+import "./styles/styles.css"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
