68e818577d91a393e22b4d2221c1b889c7dcda1e
Seanjohnferesi <seanjohnferesi27@gmail.com>
2025-12-08 21:29:02 +0200

    refactor: map category data to generate Explore page sections

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,22 +1,16 @@
 import "../styles/explore.css"
+import { categories } from "../utils/data.js"
 
 function Explore () {
     return(
         <>  
             <section className="category-container">
-
+                {categories.map(category => 
+                    <div className="category" key={category.id}>
+                        <h2>{category.title}</h2>
+                    </div>
+                )}
             </section>
-            <div className="category-1">
-
-            </div>
-
-            <div className="category-2">
-
-            </div>
-
-            <div className="category-3">
-
-            </div>
         </>
     )
 }
---
+++ src/utils/data.js
@@ -0,0 +1,16 @@
+export const categories = [
+    {
+        id: 1,
+        title: "Manicures & Pedicures",
+    },
+
+    {
+        id: 2,
+        title: "Massages",
+    },
+
+    {
+        id: 3,
+        title: "Waxing",
+    }
+]
