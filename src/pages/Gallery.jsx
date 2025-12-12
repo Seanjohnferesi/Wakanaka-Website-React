68d5a0bfc61cb9b1d9bd73b1d2c89a5010e1cccf
Seanjohnferesi <seanjohnferesi27@gmail.com>
2025-12-12 05:55:03 +0200

    style: added a grid layout gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,9 +1,16 @@
-function Gallery() {
-    return(
-        <section className="gallery-grid-container">
+import "../styles/gallery.css";
+const images = import.meta.glob('../images/gallery/*.{jpg,png,jpeg}', { eager: true });
 
+function Gallery() {
+    return (
+        <section className="gallery-grid-container">
+            {Object.values(images).map((img, i) => (
+                <div className="bento-item" key={i}>
+                    <img src={img.default} alt="" />
+                </div>
+            ))}
         </section>
-    )
+    );
 }
 
-export default Gallery
+export default Gallery;
