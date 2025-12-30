463eb6d36d3c008eb51ab639c2cbab5cc325633e
Seanjohnferesi <seanjohnferesi27@gmail.com>
2025-12-30 02:21:20 +0200

    style: added nail service section

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,21 @@
+import "../styles/main-content.css"
+import nails from "../images/background/nude-clr.jpg"
+
 function MainContent() {
     return (
-        <section className="main-content"> 
-        
+        <section className="main-content-cont"> 
+            <h1>Services</h1>
+            <div className="main-content">
+                <div className="nail-services">
+                    <h2>Nail Services</h2>
+                    <div className="nails-bg">
+                        <img src={nails} alt="" />
+                    </div>
+                </div>
+                <div className="spa-treatments">
+
+                </div>
+            </div>
         </section>
     )
 } 
--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,20 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWhatsapp, faInstagram, faFacebookF } from "@fortawesome/free-brands-svg-icons";import "../styles/floats.css"
+import { faWhatsapp, faInstagram, faFacebookF } from "@fortawesome/free-brands-svg-icons"
 import "../styles/social.css"
 
 function SocialMedia() {
     return (
         <section className="side-icons">
             <div>
-                <FontAwesomeIcon icon={faWhatsapp} className='icon1'/>
+                <a href="https://wa.me/message/M5NHR5KQTL52H1">
+                    <FontAwesomeIcon icon={faWhatsapp} className='icon1'/>
+                </a> 
             </div>
 
             <div>
-                <FontAwesomeIcon icon={faInstagram} className='icon2' />
+                <a href="https://www.instagram.com/wakanaka_beautyparlour?igsh=YW14NXpweTd5aGVq">
+                    <FontAwesomeIcon icon={faInstagram} className='icon2' />
+                </a>
             </div>
 
             <div>
-                <FontAwesomeIcon icon={faFacebookF} className='icon3' />
+                <a href="https://www.facebook.com/share/1A824gSqeu/">
+                    <FontAwesomeIcon icon={faFacebookF} className='icon3' />
+                </a>
             </div>
         </section>
     )
